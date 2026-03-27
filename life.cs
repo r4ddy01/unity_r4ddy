d16d52be3c49101921294598c303e1a3c44ae13e
Corado <alexconradojr710@gmail.com>
2026-03-27 17:57:45 -0300

    Incrementa vida ao personagem
    Fix typo in Destroy method and increment player's life.

---
+++ life.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Life : MonoBehaviour 
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.GetComponent<PlayerController>().life++;
+            Destroy(this.ganeObject);
+        }
+    }
+}
