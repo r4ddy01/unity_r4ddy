5339da2834354a018bdd3a38da96471518e53d6b
Corado <alexconradojr710@gmail.com>
2026-03-28 23:03:35 -0300

    Ataque do inimigo

---
+++ RangeEnemie.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EnimieRange : MonoBehaviour 
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            GetComponentInParent<Animator>().Play("Attack", -1);
+        }
+    }
+}
